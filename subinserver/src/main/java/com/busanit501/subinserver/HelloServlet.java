2b8213a89384a429c0c5352ff5794c4148425867
subin806 <chosb0806@naver.com>
2024-11-21 12:50:41 +0900

    --

--- subinserver/src/main/java/com/busanit501/subinserver/HelloServlet.java
+++ subinserver/src/main/java/com/busanit501/subinserver/HelloServlet.java
@@ -4,24 +4,29 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
+
+        System.out.println("HelloServlet init...최초 1번 호출됨.");
         message = "Hello World!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+        System.out.println("HelloServlet doGet...호출시 마다 매번 호출됨.");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
 
     public void destroy() {
+        System.out.println("HelloServlet destroy...소멸시 한번 호출됨.");
     }
 }
